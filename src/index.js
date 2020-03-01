2dbdb8346f4ff991235cdbc144d00c5a0a2398b4
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-03-01 08:06:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import './style.css';
 window.DatoCmsPlugin.init((plugin) => {
   plugin.startAutoResizer();
 
-  const container = document.createElement('button');
-  container.classList.add('DatoCMS-button');
-  container.classList.add('DatoCMS-button--large');
-  container.textContent = 'Zkopírovat fotky';
+  const select = document.createElement('select');
+  select.options = [];
 
-  container.addEventListener('click', () => {
+  const button = document.createElement('button');
+  button.classList.add('DatoCMS-button');
+  button.classList.add('DatoCMS-button--large');
+  button.textContent = 'Zkopírovat fotky';
+
+  button.addEventListener('click', () => {
   });
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+  container.appendChild(select);
+  container.appendChild(button);
 
   document.body.appendChild(container);
 });
