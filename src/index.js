fd42dc0fa186df7fdeea859717bc8e35acb4676a
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-03-02 10:40:03 +0100

    rewrite

--- src/index.js
+++ src/index.js
@@ -4,31 +4,66 @@ import { SiteClient } from 'datocms-client'
 window.DatoCmsPlugin.init(async (plugin) => {
   plugin.startAutoResizer()
 
-  const client = new SiteClient(plugin.parameters.global.datoCmsApiToken)
-  const items = await client.items.all({
-    'filter[type]': plugin.parameters.instance.sourceTypeId,
-  });
+  const titleField = plugin.parameters.instance.titleField
+  const allItemsQuery = plugin.parameters.instance.allItemsQuery
+  const sourceField = plugin.parameters.instance.sourceField
+  const targetField = plugin.parameters.instance.targetField
 
-  const select = document.createElement('select')
-  items.forEach(i => {
-    const option = document.createElement('option');
-    option.value = i.id;
-    option.text = i.title;
-    select.appendChild(option);
-  });
+  fetch('https://graphql.datocms.com/preview', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${plugin.parameters.global.datoCmsApiToken}`,
+    },
+    body: JSON.stringify({
+      query: `{
+            ${allItemsQuery}(orderBy: [${titleField}_ASC]) {
+              id
+              ${titleField}
+              ${sourceField} {
+                id
+              }
+            }
+          }
+          `,
+    }),
+  }).then(res => res.json())
+    .then((res) => {
+      const data = res.data[allItemsQuery]
+      const items = {}
+      const select = document.createElement('select')
 
-  const button = document.createElement('button')
-  button.classList.add('DatoCMS-button')
-  button.classList.add('DatoCMS-button--large')
-  button.textContent = 'Zkopírovat fotky'
+      data.forEach(i => {
+        const option = document.createElement('option')
+        option.value = i.id
+        option.text = i[titleField]
+        select.appendChild(option)
+        items[i.id] = i
+      })
 
-  button.addEventListener('click', () => {
-  })
+      const button = document.createElement('button')
+      button.classList.add('DatoCMS-button')
+      button.classList.add('DatoCMS-button--large')
+      button.textContent = 'Zkopírovat fotky'
 
-  const container = document.createElement('div')
-  container.classList.add('container')
-  container.appendChild(select)
-  container.appendChild(button)
+      button.addEventListener('click', () => {
+        plugin.setFieldValue(targetField, data[select.selectedIndex][sourceField].map(v => ({
+          upload_id: v.id,
+          alt: null,
+          title: null,
+          customData: {},
+        })))
+      })
 
-  document.body.appendChild(container)
+      const container = document.createElement('div')
+      container.classList.add('container')
+      container.appendChild(select)
+      container.appendChild(button)
+
+      document.body.appendChild(container)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
